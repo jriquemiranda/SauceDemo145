0325d2458f20c5aa26e7a2a73ff5be8320d96fee
jriquemiranda <jriquemiranda@gmail.com>
2025-10-01 22:14:20 -0300

    Criação de arquivos de configuração e libs privadas

--- playwright.config.js
+++ playwright.config.js
@@ -1,11 +1,43 @@
-const { defineConfig } = require('@playwright/test')
+const {defineConfig, devices} = require('@playwright/test')
+const path = require('path')
+const {computeRunFolder, ensureSubDirs} = require('./utils/path_tools_prof')         //  Ver aula anterior, para ajustar
+
+// Diretórios onde ficam os artefatos
+const ARTIFACTS_ROOT = path.join(__dirname, 'artifacts')    // Nome da pasta raíz
+const runDir = computeRunFolder(ARTIFACTS_ROOT) //  Ver aula anterior, para ajustar
+const {resultsDir, screenshotsDir} = ensureSubDirs(runDir)    //  Ver aula anterior, para ajustar
+
+// Expõe caminhos de diretórios com variáveis de ambiente
+process.env.RUN_DIR         = runDir
+process.env.SCREENSHOTS_DIR = screenshotsDir
 
 module.exports = defineConfig({
+    testDir:        '.tests',   // Diretório dos testes
+    timeout:        30000,  // 
+    fullyParallel:  true,   // Execução em paralelo
+    outputDir:      resultsDir,
     use: {
         baseURL:    'https://www.saucedemo.com',
-        headless:   false,
+        headless:   false,  // Exibe o browser
+        
+        // Politicas globais de artefatos automáticos
+        screenshot: 'only-on-failure',  // Gera imagem apenas quando houver erro
+        video:      'retain-on-failure',    // Mantem o vídeo apenas se houver erro
+        trace:      'retain-on-failure',    // Mantem o trace apenas se houver erro
+        
+        // Tipos de timeout
+        actionTimeout: 1500, // Aborta se nada acontecer em ms
+        navigationTimeout: 20000,   // Aborta se parar o navegador
+
         launchOptions:{
-            slowMo: 1000
+            slowMo: 0    // espera a cada ação em ms
         }
-    }
+    },
+    
+    projects:[
+        {
+            name:   'chromium',
+            use:    {...devices['Desktop Chrome']}
+        }
+    ]
 })
---
+++ utils/snap.js
@@ -0,0 +1,29 @@
+const fs = require('fs')
+const path = require('path')
+const playwrightConfig = require('../playwright.config')
+
+// Armazenamento local para os snapshots
+const SHOTS_DIR = process.env.SCREENSHOTS_DIR
+
+function safe_name(name){
+    // return String(name).replace(/[^\w\d-_.]+/g,'_').slice(0,120)
+    return String(name).replace(/[^\w_.-]+/g, '_').slice(0,120)
+}
+
+/**
+ * Salvar o screenshot quando solicitado, com nome amigável
+ * @param {import('@playwright/test').Page} page
+ * @param {import('@playwright/test').TestInfo} test_info
+ * @param {string} label
+*/
+
+async function snap(page, test_info, label) {
+    const file = `${safe_name(test_info.title)}__${safe_name(label)}.png`
+    const dest = path.join(SHOTS_DIR, file)
+
+    fs.mkdirSync(SHOTS_DIR, {recursive:true})
+    await page.screenshot({path:dest, fullPage:true})   // o fullPage:true vai pegar toda a página, mesmo que não dê pra ver todo na tela
+    return dest
+}
+
+module.exports={snap}   // visibilidade para todo o projeto
